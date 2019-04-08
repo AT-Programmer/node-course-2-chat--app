d2f78fb4ce8fe978bd9a0d8e35c34fe58fd85f25
Gaurav Kumar <kakkukmr50@gmail.com>
2019-04-08 11:58:40 +0100

    Adding Chat Facility

--- server/server.js
+++ server/server.js
@@ -20,10 +20,11 @@ io.on('connection' , function(socket){
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined!'))
 
-  socket.on('createMessage', function(message){
+  socket.on('createMessage', function(message,callback){
   console.log('createMessage',message)
 
     io.emit('newMessage', generateMessage(message.from,message.text));
+    callback('This Is From The Server')
 
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,                                   // For Show Message Only On Others Not For Him
