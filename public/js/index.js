f2ecdb25b3f60e74f0fc7a1aebd9c9f024cfd392
Gaurav Kumar <kakkukmr50@gmail.com>
2019-04-09 13:14:08 +0100

    Improving User Experience

--- public/js/index.js
+++ public/js/index.js
@@ -12,21 +12,32 @@
 
     socket.on('newMessage', function(message){         // For Listening Events Produced By Socket Emiting
       var formattedTime = moment().format('h:mm a')
-        console.log('newMessage',message )
-        var li = jQuery('<li></li>');
-        li.text(`${message.from}: ${formattedTime} ${message.text}`)
+      var template = jQuery('#message-template').html()
+      var test = Mustache.render(template,{
+        text : message.text,
+        from : message.from,
+        createdAt: formattedTime
+      })
+      jQuery('#messages').append(test)
 
-        jQuery('#messages').append(li)
       })
 
       socket.on('newLocationMessage', function(message){
               var formattedTime = moment().format('h:mm a')
-        var li = jQuery('<li></li>')
-        var a = jQuery('<a target="_blank">My Current Location</a>')
-        li.text(`${message.from}: ${formattedTime} `)
-        a.attr('href', message.url)
-        li.append(a)
-        jQuery('#messages').append(li)
+        // var li = jQuery('<li></li>')
+        // var a = jQuery('<a target="_blank">My Current Location</a>')
+        // li.text(`${message.from}: ${formattedTime} `)
+        // a.attr('href', message.url)
+        // li.append(a)
+        // jQuery('#messages').append(li)
+
+        var template = jQuery('#location-message-template').html();
+        var test = Mustache.render(template, {
+          from : message.from,
+          createdAt: formattedTime,
+          url: message.url
+        })
+        jQuery('#messages').append(test)
 
 
       })
